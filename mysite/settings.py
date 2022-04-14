2022bacd89c1e92712fa7b3dcccd4deab17e63df
cwon0040 <cwon0040@student.monash.edu>
2022-04-14 16:18:30 +0800

    allowedhost

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut aw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
